752c867edce8a128b595b1ad28b2cc8e1f122fdc
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-08-22 01:41:51 +0100

    Feat: Create dummy navigationData file

---
+++ components/data/navigationData.tsx
@@ -0,0 +1,84 @@
+export interface NavItem {
+  label: string;
+  url: string;
+  items?: NavItem[];
+}
+
+export const navData: NavItem[] = [
+  {
+    label: "Home",
+    url: "/",
+  },
+  {
+    label: "About",
+    url: "/about",
+  },
+  {
+    label: "Services",
+    url: "/about",
+    items: [
+      {
+        label: "Sublink 1",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+    ],
+  },
+  {
+    label: "Dropdown",
+    url: "",
+    items: [
+      {
+        label: "Sublink 1",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+      {
+        label: "Sublink 2",
+        url: "/about/about",
+      },
+    ],
+  },
+];
