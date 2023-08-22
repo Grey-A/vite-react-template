92f957eb786ac983fcc8d4df76714fc29b85e230
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-08-22 17:04:23 +0100

    Feat: Update tailwind confi file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "400px",
+        "3xl": "2500px",
+      },
+      colors: {
+        subText: "#676C7A",
+        primaryBrand: "#4E4BED",
+        primaryBrandColor: "#0E0D35",
+
+        // input colors
+
+        inputPrimary: "#4E4BED",
+        inputSecondary: "#FFB13D",
+        inputText: "#0E0D35",
+        inputPlaceholderTextColor: "#C6C8CD",
+        inputBg: "#FEFEFE",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
